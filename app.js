528780a101ce6e02122e50ef5d8b632b179fc482
Usama Rahman <rahmanusama390@gmail.com>
2023-10-19 23:52:34 +0600

    Continue

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
+// set up copyright footer date 
+// set date
+const date = document.querySelector('.date');
+date.innerHTML = new Date().getFullYear();
+// Header navbar
+// close links
+const navToggle = document.querySelector('.nav-toggle');
+const linkContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
-// ********** set date ************
+navToggle.addEventListener('click', function(){
+    // manuall
+    // linkContainer.classList.toggle("show-links");
+    // Dynamic
+    const containerHeight = linkContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    if(containerHeight === 0){
+        linkContainer.style.height = `${linksHeight}px`
+    }else{
+        linkContainer.style.height = 0;
+    }
+})
+// Fixed Navbar
+const navbar = document.querySelector("#nav")
+const topLink = document.querySelector(".top-link")
 
-// ********** close links ************
-
-// ********** fixed navbar ************
-
-// ********** smooth scroll ************
-// select links
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navbar.classList.add("fixed-nav")
+    }else{
+        navbar.classList.remove("fixed-nav")
+    }
+    if(scrollHeight > 500){
+        topLink.classList.add
+    }
+})
